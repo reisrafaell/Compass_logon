1b216e04c62b3239e5454a5965eee1629f6f623a
Rafael Reis <rafaeloliveira63@yahoo.com>
2022-02-27 20:09:37 -0300

    Feat: adding couter logic

--- src/components/stopWatch/index.vue
+++ src/components/stopWatch/index.vue
@@ -1,36 +1,47 @@
 <template>
- <div id="center">
-      <modelTitle setClassdiv="textFooterTwee" setClass="" size="font-size: clamp(0.8rem, 2.5vw, 4rem);" type="h3" :text="textContentTwee" />
-      <p>secunds</p>
-  
- </div>
+  <div id="center">
+    <modelTitle
+      setClassdiv="textFooterTwee"
+      setClass=""
+      size="font-size: clamp(0.8rem, 2.5vw, 4rem);"
+      type="h3"
+      :text="textContentTwee"
+    />
+    <p>secunds</p>
+  </div>
 </template>
 <script>
-import modelTitle from "@/components/title/index.vue"
+import modelTitle from "@/components/title/index.vue";
 
 export default {
   name: "stopwatch",
-  components:{
-    modelTitle
+  components: {
+    modelTitle,
   },
   data() {
-    return {  
-      actualClock:"",
+    return {
+      actualClock: "",
       dayWeek: "",
-      textContentTwee: '600',
-
+      textContentTwee: "600",
     };
   },
   methods: {
-   
- 
+    stopWatch: function () {
+      let time = this;
+      let initTimer = 600;
+      setInterval(function () {
+        time.textContentTwee = initTimer;
+        initTimer--;
+        if (initTimer === 0) {
+          time.$router.push({ name: "Login" });
+        }
+      }, 1000);
+    },
   },
-  mounted(){
-    this.stopWatch()
-  }
-
-    } 
-
+  mounted() {
+    this.stopWatch();
+  },
+};
 </script>
 <style lang="sass" scoped>
 @import './index.scss'
