fe25ce2ee4763363fbb76283b4aa8c0e2a03fbac
Rafael Reis <rafaeloliveira63@yahoo.com>
2022-03-01 00:02:04 -0300

    fix: added anhancements to coding patternadde

---
+++ src/components/textContent/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="setClassdiv"  >
+    <p :style="size" :class="setClass" v-if="type === 'p'">
+      {{ text }}
+     
+    </p>  
+   
+  </div>
+</template>
+<script>
+export default {
+  name: "textContent",
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+   
+    setClass: {
+      type: String,
+    },
+    setClassdiv:{
+      type:String,
+      default:""
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
--- src/containers/homeContainerFooter/index.vue
+++ src/containers/homeContainerFooter/index.vue
@@ -1,26 +1,22 @@
 <template>
   <div>
-    <modelTitle
-      setClassdiv="textFooter"
-      setClass=""
-     
-      type="h3"
+    <textContent
+      setClassdiv="textFooter"         
+      type="p"
       :text="textContent"
     />
 
-    <modelTitle
-      setClassdiv="textFooterTwo"
-      setClass="aaa"
+    <textContent
+      setClassdiv="textFooterTwo"    
       size="font-size: clamp(0.8rem, 0.5vw, 1.5rem);"
-      type="h3"
+      type="p"
       :text="textContentTwo"
     />
     <stopwatch />
-    <modelTitle
-      setClassdiv="navegation"
-      setClass=""
+    <textContent
+      setClassdiv="navegation"    
       size="font-size: clamp(0.8rem, 0.5vw, 1.5rem);"
-      type="h3"
+      type="p"
       :text="navegation"
     />
     <buttonLogout />
@@ -28,16 +24,18 @@
 </template>
 
 <script >
-import modelTitle from "@/components/title/index.vue";
-import stopwatch from "../../components/stopWatch/index.vue";
-import buttonLogout from "../../components/logout/index.vue";
+
+import stopwatch from "@/components/stopWatch/index.vue";
+import buttonLogout from "@/components/logout/index.vue";
+import textContent from "@/components/textContent/index.vue"
 
 export default {
   name: "homeContainerFooter",
   components: {
-    modelTitle,
+  
     stopwatch,
     buttonLogout,
+    textContent
   },
   data() {
     return {
